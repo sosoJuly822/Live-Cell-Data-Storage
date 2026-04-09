a82fd2bc8db47303be82036d51bf1de96be170bc
sosoJuly822 <535473541@qq.com>
2026-04-09 16:23:18 +0800

    remove unused files

--- 1_Sequence_matching/result_output.sh
+++
@@ -1,6 +0,0 @@
-python analysis_result.py \
-    --lib_path /home/liuycomputing/wsp_sequencing/FINAL.xlsx \
-    --file_folder /home/liuycomputing/wsp_sequencing/seq_20250115/ \
-    --error_file_name whole_error_dict_read2_20250117.pkl \
-    --per_seq_filename per_item_num_read2.csv \
-    --per_item_filename per_item_error_rate_read2.csv
